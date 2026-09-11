c6f64f3b9b81ddc9d92c2faccae49e9ad60adaa3
Marc-Oliver Blumenauer <122913485+TheDiyDude@users.noreply.github.com>
2026-09-12 00:03:09 +0200

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,16 @@
+    // SRTLA_BACKEND_OR_SERVER
+	// Legacy stats API:
+	// like http://IP_OF_YOUR_SRTLA_RELAY:3000/stats?streamer=USERNAME&key=PASSWORD
+	// PORT here is 3000 of legacy stats URL
+	// USERNAME --> streamer=USERNAME
+	// PASSWORD --> key=PASSWORD
+    const SRTLA_STATS_IP = "IP_OF_YOUR_SRTLA_RELAY";
+    const SRTLA_STATS_PORT = "3000";
+    const SRTLA_STATS_USER = "USERNAME";
+    const SRTLA_STATS_KEY = "PASSWORD";
+
+    // Traccar API Backend
+	const TRACCAR_URL = "http://localhost:8082";
+    const TRACCAR_USER = "USERNAME_OR_EMAIL";
+    const TRACCAR_PASS = "PASSWORD";
+    const TRACCAR_DEVICE_ID = "DEVICE_ID"; 
